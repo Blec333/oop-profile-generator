63e1081d26c6d9636d8a4a470ad747bbac68fde8
BLEC333 <bleclair3@gmail.com>
2022-04-23 11:31:02 -0700

    add employee tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,85 +1,58 @@
-// The application must include `Employee`, `Manager`, `Engineer`, and `Intern` classes. The tests for these classes (in the `_tests_` directory) must ALL pass.
 
 // The first class is an `Employee` parent class with the following properties and methods:
-
-// * `name`
-
-// * `id`
-
-// * `email`
-
-// * `getName()`
-
-// * `getId()`
-
-// * `getEmail()`
-
-// * `getRole()`&mdash;returns `'Employee'`
-
-// The other three classes will extend `Employee`.
-
-// In addition to `Employee`'s properties and methods, `Manager` will also have the following:
-
-// * `officeNumber`
-
-// * `getRole()`&mdash;overridden to return `'Manager'`
-
-// In addition to `Employee`'s properties and methods, `Engineer` will also have the following:
-
-// * `github`&mdash;GitHub username
-
-// * `getGithub()`
-
-// * `getRole()`&mdash;overridden to return `'Engineer'`
-
-// In addition to `Employee`'s properties and methods, `Intern` will also have the following:
-
-// * `school`
-
-// * `getSchool()`
-
-// * `getRole()`&mdash;overridden to return `'Intern'`
-
-// Finally, although it’s not a requirement, consider adding validation to ensure that user input is in the proper format.
 
 const Employee = require("../lib/Employee");
 
 describe("Employee class", () => {
-  it("name", () => {
-
-    expect(new Employee("?").visible).toBe(true);
-  });
-  
-  it("id", () => {
-
-    expect(new Employee("a").visible).toBe(false); 
-  });
-
-  it("email", () => {
-
-    expect(new Employee("a").toString()).toBe("_");
-  });
-
-  it("getName", () => {
-
-    expect(new Employee("?").toString()).toBe("?");
-  });
-
-  describe("guess", () => {
-    it("Correct guess returns true", () => {
-
-      expect(new Employee("j").guess("j")).toBe(true);
+// * `name`
+    it("name", () => {
+        const name = 'John';// Arrange
+        const obj = new Employee(name);// Act
+        expect(obj.name).toEqual(name);// Assert
     });
 
-    it("Incorrect guess returns false", () => {
-
-      expect(new Employee("j").guess("l")).toBe(false);
+// * `id`
+    it("id", () => {
+        const id = '123';// Arrange
+        const obj = new Employee(id);// Act
+        expect(obj.id).toEqual(id);// Assert
     });
-  });
 
-  describe("getSolution", () => {
-    it("returns character", () => {
-      expect(new Employee("l").getSolution()).toBe("l");
+// * `email`
+    it("email", () => {
+        const email = 'some@thing.com';// Arrange
+        const obj = new Employee(email);// Act
+        expect(obj.email).toEqual(email);// Assert
     });
-  });
+
+// * `getName()`
+    it("getName", () => {
+        const name = 'John';// Arrange
+        const obj = new Employee(name);// Act
+        expect(getName()).toEqual(name);// Assert
+    });
+
+// * `getId()`
+    it("getID", () => {
+        const id = '123';// Arrange
+        const obj = new Employee(id);// Act
+        expect(getID()).toEqual(id);// Assert
+    });
+
+// * `getEmail()`
+    it("getEmail", () => {
+        const email = 'some@thing.com';// Arrange
+        const obj = new Employee(email);// Act
+        expect(getEmail()).toEqual(email);// Assert
+    });
+
+// * `getRole()` returns `'Employee'`
+    it("getRole", () => {
+        const role = 'Employee';// Arrange
+        const obj = new Employee(role);// Act
+        expect(getEmail()).toEqual(role);// Assert
+    });
+
+
+// Finally, although it’s not a requirement, consider adding validation to ensure that user input is in the proper format.
 });
